f754ef0d7598aa008e5be82a8496c7bc4f98b2fc
Nguyen Quang Cuong <cuongnqpd08037@fpt.edu.vn>
2023-03-09 09:04:21 +0700

    noplab1

---
+++ Lab1/app.js
@@ -0,0 +1,10 @@
+//Nhập vào 2 số nguyên a và b từ bàn phím
+let a = parseInt(prompt("Nhập số nguyên a: "));
+let b = parseInt(prompt("Nhập số nguyên b: "));
+
+//Tính tổng c của a và b (c = a + b)
+let c = a + b;
+
+//In ra kết quả đồng thời bằng 2 cách
+document.write("Tổng của " + a + " và " + b + " là: " + c);
+console.log("Tổng của " + a + " và " + b + " là: " + c);
