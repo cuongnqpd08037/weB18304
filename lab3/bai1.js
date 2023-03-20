85f91e7a335846ed9f622a71f7a68d617a44f553
Nguyen Quang Cuong <cuongnqpd08037@fpt.edu.vn>
2023-03-20 14:24:01 +0700

    noplab3

--- lab3/bai1.js
+++ lab3/bai1.js
@@ -1,3 +1,26 @@
+let choice;
+do {
+  choice = parseInt(
+    prompt(
+      "Nhap lua chon:\n1. Tinh diem trung binh (switch case)\n2. Tinh diem trung binh (if else)\n0. Thoat"
+    )
+  );
+
+  switch (choice) {
+    case 1:
+      tinhDiemTrungBinhSwitch();
+      break;
+    case 2:
+      tinhDiemTrungBinhIfElse();
+      break;
+    case 0:
+      alert("Tam biet!");
+      break;
+    default:
+      alert("Lua chon khong hop le!");
+      break;
+  }
+} while (choice != 0);
 function tinhDiemTrungBinhIfElse() {
   let toan = parseFloat(prompt("Nhap diem Toan: "));
   let ly = parseFloat(prompt("Nhap diem Ly: "));
